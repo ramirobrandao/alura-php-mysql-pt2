a78848a3fe7e5eae1d7c9ab71efb775332a516a6
ramirobrandao <ramirobrando@outlook.com>
2017-03-17 11:17:17 -0300

    :construction: alteração no arquivo

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php include("logica-usuario.php");
 	logout();
-	$_SESSION["success"] = ["Deslogado com sucesso"];
+	$_SESSION["success"] = "Deslogado com sucesso";
 	header("Location: index.php");
 	die();
 ?>
